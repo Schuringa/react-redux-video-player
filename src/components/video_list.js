16928d4439cb20af1640a5e32b617ece1a211408
jordans <schuringa.jordan@gmail.com>
2017-08-29 14:32:21 +1000

    Added video list componenet and video list item component.

---
+++ src/components/video_list.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import VideoListItem from './video_list_item'
+
+const VideoList = props => {
+  const videoItems = props.videos.map(video => {
+    return <VideoListItem video={video} />
+  })
+
+  return (
+    <ul className='col-md-4 list-group'>
+      {videoItems}
+    </ul>
+  )
+}
+
+VideoList.propTypes = {
+  videos: PropTypes.array
+}
+
+export default VideoList
