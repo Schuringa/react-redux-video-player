734e8e1ad2fd5b8a91d691fd6d55061c652e75ec
jordans <schuringa.jordan@gmail.com>
2017-08-29 15:12:10 +1000

    Added video detail component. Added video list item component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyAxoNCB4sHVFXkVshnF5QY1AnaSuu8xJrs'
 
@@ -11,7 +12,10 @@ class App extends Component {
   constructor (props) {
     super(props)
 
-    this.state = { videos: [] }
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
 
     YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
       this.setState({ videos })
@@ -22,6 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     )
